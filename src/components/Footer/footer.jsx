cf128976ec8106e1d7162537926622ea7e9ddc51
Oknown15 <buchijunior90@gmail.com>
2024-05-24 03:02:14 +0100

    new changes

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import './footer.css'
+
+function footer(props) {
+    return (
+        <div className='container-xxl'>
+            <div className='Join'>
+                <div className='text-light p-3 d-md-flex align-items-center justify-content-between'>
+                    <div>
+                        <p className='m-0'>Join X</p>
+                        <p>Receive pricing updates, shopping tips & more!</p>
+                    </div>
+                    <div className='border border-dark input rounded-5 px-3 py-3'>
+                        <input className=' border-0' placeholder='Your email address'/>
+                        <a className='py-3  px-4 text-decoration-none text-light rounded-5 p-3' href='#'>Submit</a>
+                    </div>
+                </div>
+                <div className='d-md-flex gap-5 border-top border-dark my-3 py-4 justify-content-evenly'>
+                    <div className='p-2 Company'>
+                        <div className=''>
+                            <ul className='list-unstyled m-0 py-2 px-2 footerLink'>
+                                <h4 className='text-light m-0 text-justify fs-5'>Company</h4>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">About us</a>
+                                </li>
+                                 <li>
+                                    <a  className='text-light text-decoration-none' href="">Blog</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Services</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">FAQs</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Terms</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='p-2 Company'>
+                        <h4 className='text-light m-0 fs-5'>Quick Links</h4>
+                        <div className=''>
+                            <ul className='list-unstyled m-0 py-2 px-2 footerLink'>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Get in Touch</a>
+                                </li>
+                                 <li>
+                                    <a  className='text-light text-decoration-none' href="">Help center</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Live chat</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">How it works</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='p-2 Company'>
+                        <h4 className='text-light m-0 fs-5'>Vehicle Type</h4>
+                        <div className=''>
+                            <ul className='list-unstyled m-0 py-2 px-2 footerLink'>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Model S</a>
+                                </li>
+                                 <li>
+                                    <a  className='text-light text-decoration-none' href="">Model Y</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Model 3</a>
+                                </li>
+                                 <li>
+                                    <a className='text-light text-decoration-none' href="">Tesla Semi</a>
+                                </li>
+                                <li>
+                                    <a className='text-light text-decoration-none' href="">Cybertruck</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='p-2 Company'>
+                        <h4 className='text-light m-0 fs-5'>Our mobile App</h4>
+                        <div className='my-3'>
+                            <ul className='list-unstyled text-light m-0 p-1 footerLink'>
+                                <div className='row Apple p-2 rounded-4 py-3'>
+                                    <div className="col-md-6 w-25 d-md-flex justify-content-center">
+                                        <i className="fs-3 bi bi-apple"></i>
+                                    </div>
+                                    <div className="col">
+                                        <div className="col">
+                                            <p className='m-0'>Download in the</p>
+                                        </div>
+                                        <div className="col">
+                                            <p className='m-0 fw-bold'>Apple Store</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ul>
+                        </div>
+                        <div className='my-3'>
+                            <ul className='list-unstyled text-light m-0 p-1 footerLink'>
+                                <div className='row Store p-2 rounded-4 py-3'>
+                                    <div className="col-md-6 w-25 d-md-flex justify-content-center">
+                                        <i className="fs-3 bi bi-google-play"></i>
+                                    </div>
+                                    <div className="col">
+                                        <div className="col">
+                                            <p className='m-0'>Download in the</p>
+                                        </div>
+                                        <div className="col">
+                                            <p className='m-0 fw-bold'>Apple Store</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ul>
+                        </div>
+                        <div className='d-flex d-md-block justify-content-end'>
+                            <p className=' m-0 fs-5 text-light Contact fw-semibold'>Contact with Us</p>
+                        </div>
+                        <div className='d-md-flex d-flex justify-content-end gap-5 my-3'>
+                            <div>
+                            <i className="text-light fab fa-facebook-f"></i>
+                            </div>
+                            <div>
+                                <i className='fab text-light fa-twitter'></i>
+                            </div>
+                            <div>
+                                <i className='fab text-light fa-instagram'></i>
+                            </div>
+                            <div>
+                                <i className='fab text-light fa-linkedin-in'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='border-top text-light border-dark'>
+                   <div className='d-md-flex my-2 justify-content-between'>
+                        <div>
+                            <p className='m-0'>© 2024 exemple.com. All rights reserved.</p>
+                        </div>
+                        <div className='d-md-flex Terms align-items-center gap-2'>
+                            <a className='text-decoration-none text-light' href="">Terms & conditions</a> 
+                            <i className='dot fas fa-circle'></i>
+                            <a className='text-decoration-none pe-3 text-light'  href="">Privacy Notice</a>
+                        </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default footer;
---
+++ src/components/Hero/hero.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import Navbar from '../Navbar/navbar'
+import Teslabg from './images/TeslaCar.avif';
+import './styles/hero.css'
+
+function hero(props) {
+    return (
+        <div>
+            <div className='container'>
+                <Navbar/>
+                <div id="carouselExampleCaptions" className="carousel slide ">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={Teslabg} className="HeroBg d-block w-100" alt="..."/>
+                            <div className="carousel-caption Caption my-1">
+                                <h5 className='text-dark '>Find cars for sale and for rent near you</h5>
+                                <p>A Vehicle For Every Lifestyle Today</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='Tabs-content  m-2'>
+                    <div className='tabs'>
+                        <ul className='d-flex gap-2 d-md-flex'>
+                            <li><a href=''>all</a></li>
+                            <li><a href=''>New</a></li>
+                            <li><a href=''>Used</a></li>
+                        </ul>
+                    </div>
+                    <div className='container btn-group'>
+                        <div className='row'>
+                            <div className='col-6 col-md-3 drop-one dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false">
+                                <h1>Select makes</h1>
+                                <p>audi</p>
+                            </div>
+                            <ul className='dropdown-menu'></ul>
+                            <div className='col-6 col-md-3 drop-two dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false">
+                                <h1>Select models</h1>
+                                <p>Q7</p>
+                            </div>
+                            <div className='col-6 col-md-3 drop-three dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false">
+                                <h1>Year</h1>
+                                <p>0 - 2024</p>
+                            </div>
+                            <div className='col-6 col-md-3 drop-three dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false">
+                                <h1>Select price</h1>
+                                <p className='text-capitalize'>all prices</p>
+                            </div>
+                            <div className='col-9 Search mx-auto rounded-4 p-2'>
+                                <a className='d-flex text-decoration-none text-light' href=''>
+                                    <i className="bi bi-search"></i>
+                                    <p className='px-2 m-0'>Search</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                <div className='row  Cards my-4 d-md-flex align-items-center'>
+                    <div className="card border-0" style={{width: "18rem"}}>
+                        <div className="card-body card-One">
+                            <h5 className="card-title">3 Cars</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Model X</h6>
+                        </div>
+                    </div>
+                    <div className="card border-0" style={{width: "18rem"}}>
+                        <div className="card-body cardThree">
+                            <h5 className="card-title">3 Cars</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Model 3</h6>
+                        </div>
+                    </div>
+                    <div className="card border-0" style={{width: "18rem"}}>
+                        <div className="card-body cardTwo">
+                            <h5 className="card-title">3 Cars</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Model S</h6>
+                        </div>
+                    </div>
+                    <div className="card border-0" style={{width: "18rem"}}>
+                        <div className="card-body cardFive">
+                            <h5 className="card-title text-light">3 Cars</h5>
+                            <h6 className=" card-subtitle mb-2 text-light">Model X</h6>
+                        </div>
+                    </div>      
+                    <div className="card border-0" style={{width: "18rem"}}>
+                        <div className="card-body cardFour">
+                            <h5 className="card-title">3 Cars</h5>
+                            <h6 className=" text-light card-subtitle mb-2 text-body-secondary">Model S convertible</h6>
+                        </div>
+                    </div>
+                    <div className="card border-0 mx-1" style={{width: "18rem"}}>
+                        <div className="card-body cardSix">
+                            <h5 className="card-title text-light">CarX</h5>
+                            <h6 className=" card-subtitle mb-2 text-light">CyberTrunk</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default hero;
